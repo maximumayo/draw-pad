fc0df81fe5d80ec6398825c2ecb4084799e639a2
maximumayo <maxgmayo@gmail.com>
2016-04-19 18:39:25 -0700

    styled buttons and input, added shortcut icon

--- sketch.js
+++ sketch.js
@@ -1,34 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    for(var i=0; i < 256; i++){
+    for (var i = 0; i < 256; i++) {
         $('#container').append('<div class="tile"></div>');
     }
 
-    $('.tile').hover(function(){
-            var red = Math.floor((Math.random() * 254) + 1);
-            var green = Math.floor((Math.random() * 254) + 1);
-            var blue = Math.floor((Math.random() * 254) + 1);
-            $(this).css('background-color', 'rgb(' + red + ',' + green + ',' + blue + ')');
-        });
+    $('.tile').hover(function () {
+        var red = Math.floor((Math.random() * 254) + 1);
+        var green = Math.floor((Math.random() * 254) + 1);
+        var blue = Math.floor((Math.random() * 254) + 1);
+        $(this).css('background-color', 'rgb(' + red + ',' + green + ',' + blue + ')');
+    });
 
-    $('#new').click(function() {
+    $('#new').click(function () {
         if ($('#rows').val() === ("")) {
             $('.tile').css('background-color', 'white');
         }
-        else { newPad(); }
+        else {
+            newPad();
+        }
     });
 
-    $('#clear').click(function(){
+    $('#clear').click(function () {
         $('.tile').css('background-color', 'white');
     });
 
 });
 
-function newPad(){
+function newPad() {
     var gridSize = $('#rows').val();
     $('#container').find('.tile').remove();
 
-    for(var i=0; i < gridSize * gridSize; i++){
+    for (var i = 0; i < gridSize * gridSize; i++) {
         $('#container').append('<div class="tile"></div>');
     }
 
@@ -37,10 +39,10 @@ function newPad(){
     $('.tile').css('width', square);
     $('.tile').css('height', square);
 
-    $('.tile').hover(function(){
-            var red = Math.floor((Math.random() * 254) + 1);
-            var green = Math.floor((Math.random() * 254) + 1);
-            var blue = Math.floor((Math.random() * 254) + 1);
-            $(this).css('background-color', 'rgb(' + red + ',' + green + ',' + blue + ')');
-        });
+    $('.tile').hover(function () {
+        var red = Math.floor((Math.random() * 254) + 1);
+        var green = Math.floor((Math.random() * 254) + 1);
+        var blue = Math.floor((Math.random() * 254) + 1);
+        $(this).css('background-color', 'rgb(' + red + ',' + green + ',' + blue + ')');
+    });
 }
